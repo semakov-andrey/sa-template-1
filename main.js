33225cec168f7debfff315626ffde20b8c68ccbf
Semakov Andrey <semakov.andrey.web@yandex.ru>
2018-07-24 20:08:42 +0400

    1.1.1

--- main.js
+++ main.js
@@ -7,9 +7,30 @@ const ncp             = require('ncp').ncp;
 let template          = require(path.resolve(__dirname, 'template.json'));
 let packageJSON       = require(path.resolve(__dirname, project, 'package.json'));
 
-packageJSON.devDependencies   = Object.assign(template.devDependencies, packageJSON.devDependencies);
-packageJSON.scripts = template.scripts;
-fs.writeFile('package.json', JSON.stringify(packageJSON, null, 2), 'utf8', error => {
+//if(!packageJSON.config) packageJSON.config = {};
+//if(!packageJSON.scripts) packageJSON.scripts = {};
+
+//packageJSON.config            = Object.assign(template.config, packageJSON.config);
+//packageJSON.scripts           = Object.assign(template.scripts, packageJSON.scripts);
+//packageJSON.devDependencies   = Object.assign(template.devDependencies, packageJSON.devDependencies);
+
+let json = {
+  ...packageJSON,
+  devDependencies: {
+    ...packageJSON.devDependencies,
+    ...template.devDependencies
+  },
+  config: {
+    ...packageJSON.config,
+    ...template.config,
+    directories: {
+      ...packageJSON.config.directories,
+      ...template.config.directories,
+    },    
+  }
+}
+
+fs.writeFile('package.json', JSON.stringify(json, null, 2), 'utf8', error => {
   if(error) return console.error('Error: ' + error);
   console.log('Success: configuration updated');
 });
