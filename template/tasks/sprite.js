ffdada7db694dcbf4fdee7ded6867fb09ef9e5a7
Semakov Andrey <semakov.andrey.web@yandex.ru>
2018-07-28 19:20:09 +0400

    commit

--- template/tasks/sprite.js
+++ template/tasks/sprite.js
@@ -1,27 +1,38 @@
 'use strict';
 
 const svgSprite       = require('gulp-svg-sprites');
+const svgo            = require('gulp-svgo');
 
 module.exports = params => {
-  let { gulp, source, target, browserSync } = params;
+  let { gulp, production, source, target, browserSync } = params;
+  let template = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="0" height="0" style="position: absolute;">
+<% _.forEach(svg, function(svgItem) { %>
+  <symbol id="<%= svgItem.name %>" viewBox="<%= svgItem.viewBox %>"<%= (svgItem.preserveAspectRatio ? ' preserveAspectRatio="' + svgItem.preserveAspectRatio + '"' : '') %>>
+    <%= svgItem.data.replace(/<svg.*?>(.*?)<\\/svg>/, "$1") %>
+  </symbol>
+<% }); %>
+</svg>`;
   gulp.task('sprite', () => {
     return gulp.src(source + '/_images/_svg/*.svg')
-      .pipe(svgSprite({
-        mode: 'symbols',
-        svg: { symbols: 'sprite.svg' },
-        preview: false,
-        asyncTransforms: true,
-        transformData: (data, config, done) => {
-          data.svg = data.svg.map(function(item) {
-            let preserveAspectRatio = /preserveAspectRatio=\"(.*?)\"/.exec(item.data);
-            item.preserveAspectRatio = preserveAspectRatio ? preserveAspectRatio[1] : 'xMidYMid'; 
-            return item;          
-          });
-          done(data);
-        },
-        templates: { symbols: require('fs').readFileSync('./tasks/templates/symbols.svg', 'utf-8') }
-      }))
-      .pipe(gulp.dest(target + '/images/'))
-      .on('end', browserSync.reload);
+    .pipe(svgo())
+    .pipe(svgSprite({
+      mode: 'symbols',
+      svg: { symbols: 'sprite.svg' },
+      preview: false,
+      asyncTransforms: true,
+      transformData: (data, config, done) => {
+        data.svg = data.svg.map(item => {
+          let preserveAspectRatio = /preserveAspectRatio=\"(.*?)\"/.exec(item.data);
+          if(preserveAspectRatio) item.preserveAspectRatio = preserveAspectRatio[1]; 
+          return item;          
+        });
+        done(data);
+      },
+      templates: {
+        symbols: production ? template.replace(/[\r\n]/g, '').replace(/[\s]+\s\</g, '<') : template
+      }
+    }))
+    .pipe(gulp.dest(target + '/images/'))
+    .on('end', browserSync.reload);
   });
 };
