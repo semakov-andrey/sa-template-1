56f8a9ced868e99e3901a0ee503888950a708ef9
Семаков Андрей <asemakov@htc-cs.ru>
2018-12-25 11:25:17 +0400

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@
 
 import Icon             from '../modules/icon/icon';
 import Preloader        from '../modules/preloader/preloader';
+import Burger           from '../modules/burger/burger';
 
 new Icon();
 new Preloader();
+new Burger();
