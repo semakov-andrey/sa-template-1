5fcc79748cadb46949440273c8d2687292ed6134
Semakov Andrey <semakov.andrey.web@yandex.ru>
2019-08-03 15:05:54 +0400

    update template

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,22 @@ const notify                    = require('gulp-notify');
 const gulpif                    = require('gulp-if');
 const sourcemaps                = require('gulp-sourcemaps');
 const browserSync	              = require('browser-sync');
-
 const args                      = minimist(process.argv.slice(2));
-const production                = typeof args.production !== 'undefined';
-const source                    = packageJSON.config.directories.source;
-const target                    = packageJSON.config.directories[production ? 'production' : 'development'];
-const dirs                      = packageJSON.config.directories.tasks;
-const entries                   = packageJSON.config.entries;
-const work                      = packageJSON.config.tasks;
+const isProd                    = typeof args.production !== 'undefined';
+
+const {
+  config: {
+    directories: { source, development, production, tasks: dirs },
+    entries,
+    tasks
+  }
+} = packageJSON;
+const target = isProd ? production : development;
 
 glob.sync('./tasks/**/*.js').map(file => require(file)({
   packageJSON,
   gulp,
-  production,
+  isProd,
   source,
   target,
   dirs,
@@ -33,8 +36,8 @@ glob.sync('./tasks/**/*.js').map(file => require(file)({
   sourcemaps
 }));
 
-gulp.task('serve', gulp.series('clean', gulp.parallel(...work), gulp.parallel('watch', 'browser')));
+gulp.task('serve', gulp.series('clean', gulp.parallel(...tasks), gulp.parallel('watch', 'browser')));
 
-gulp.task('build', gulp.series('clean', gulp.parallel(...work)));
+gulp.task('build', gulp.series('clean', gulp.parallel(...tasks)));
 
 gulp.task('default', gulp.series('serve'));
